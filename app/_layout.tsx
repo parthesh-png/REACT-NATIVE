ac5c3a3ece0d7c36f2c5dee1141c907866d3ce45
totykhan.2001@gmail.com <partheshsingh444@gmail.com>
2026-06-01 14:50:01 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,40 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity } from "react-native";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView>
+      <View style={{ padding: 16 }}>
+        <Text> hi there this is parthesh singh </Text>
+        <TextInput
+          placeholder="search city"
+          placeholderTextColor="#999"
+          style={{
+            borderWidth:1,
+            borderColor:"#ddd",
+            borderRadius:8,
+            padding:10,
+            marginTop:12
+          }}
+        ></TextInput>
+
+
+        <TouchableOpacity
+        onPress={()=>alert("searching...")}
+        style={{
+          backgroundColor:"#2563EB",
+          padding:12,
+          borderRadius:8,
+          marginTop:10,
+          alignItems:"center"
+        }}>
+
+          <Text style={{color:"white",fontWeight:"bold"}}
+          >search</Text>
+
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text> hi there this is parthesh singh </Text>
+    </View>
+  );
+}
